84bc0fdaea239bced85130cd88f3ed61024a0f54
Kevin <kevinreilly25@gmail.com>
2026-02-15 19:34:20 -0600

    Updated method comments

--- src/Lab03.java
+++ src/Lab03.java
@@ -1,6 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Name: Kevin Reilly
+ * KUID: 3195250
+ * Lab Session: Wednesday 4:30pm
+ * Lab Assignment: Lab 3
+ * 
+ * Class takes two binary Matrices and mulitplies them
+ * Collaborators: None
+ */
 public class Lab03 {
   private static final Scanner in = new Scanner(System.in);
   
@@ -9,6 +18,9 @@ public class Lab03 {
 
   }
   
+  /*
+   * Prints resulting Matrix to console.
+   */
   public static void printBooleanResult(ArrayList<ArrayList<Integer>> matrix) {
     for(int row = 0; row < matrix.size(); row++) {
       for(int col = 0; col < matrix.get(row).size(); col++) {
@@ -28,6 +40,9 @@ public class Lab03 {
     return buildMatrix();
   }
   
+  /*
+   * Builds a Matrix from user input.
+   */
   public static ArrayList<ArrayList<Integer>> buildMatrix() {
     ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
 
@@ -46,6 +61,9 @@ public class Lab03 {
     return matrix;
   }
 
+  /*
+   * Multiplies two Matrices.
+   */
   public static ArrayList<ArrayList<Integer>> multiplyMatrices(
       ArrayList<ArrayList<Integer>> matrixA, 
       ArrayList<ArrayList<Integer>> matrixB){
@@ -59,9 +77,9 @@ public class Lab03 {
       ArrayList<Integer> row = new ArrayList<>();
       for(int j = 0; j < rowsB; j++) {
         int product = 0;
-        for(int k = 0; k < colsA; k++) {
-          product += matrixA.get(i).get(k) * matrixB.get(k).get(j);
-        }
+          for(int k = 0; k < colsA; k++) {
+            product += matrixA.get(i).get(k) * matrixB.get(k).get(j);
+          }
         if(product > 1) { //Booleanize
           product = 1;
         }
